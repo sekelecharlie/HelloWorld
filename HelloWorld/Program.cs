6669e521f5bf07482f2db91555c25d1a2e5bafb9
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-11 09:34:12 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Charlie");
+            Console.WriteLine("welcome to C#");
+
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine(" ----  ");
+
+        }
+    }
+}
